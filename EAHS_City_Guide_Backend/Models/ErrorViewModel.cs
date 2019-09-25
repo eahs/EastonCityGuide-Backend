8683deb5f080583bf4cc60c47424a817c90a89ec
Tajvir Chahal <tajvirchahal87@gmail.com>
2019-09-25 12:43:35 -0400

    Project Creation

---
+++ EAHS_City_Guide_Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EAHS_City_Guide_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
