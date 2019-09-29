66b0bc41719de829619c970c2b74ecd47e3ef5e4
Tajvir C <tajvirchahal87@gmail.com>
2019-09-29 15:02:52 -0400

    Added LocationViewModel

--- ADSBackend/Controllers/LocationController.cs
+++ ADSBackend/Controllers/LocationController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ADSBackend.Data;
+using ADSBackend.Models.AdminViewModels;
+using ADSBackend.Models.Identity;
+using ADSBackend.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ADSBackend.Controllers
 {
     public class LocationController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly IEmailSender _emailSender;
+
+        public LocationController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, IEmailSender emailSender)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var locations = await _context.Users.OrderBy(x => x.LastName).ToListAsync();
+            
+
+            //Console.WriteLine(users);
+
+            var locationModel = locations.Select(x => new LocationsViewModel
+            {
+                Id = 29391203,
+                Email = "example@gmail.com",
+                Title = "boi",
+                Latitude = 5.0f,
+                Longitude = 7.0f,
+            }).ToList();
+
+
+            return View(locationModel);
         }
     }
 }
---
+++ ADSBackend/Models/AdminViewModels/LocationsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models.AdminViewModels
+{
+    public class LocationsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Latitude")]
+        [Required]
+        public float Latitude { get; set; }
+
+        [Display(Name = "Longitude")]
+        [Required]
+        public float Longitude { get; set; }
+
+        [Display(Name = "PhoneNumber")]
+        [Required]
+        public float PhoneNumber { get; set; }
+    }
+}
